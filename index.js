0091a6f9ac9c047410d8fa903bf81dd8b5cbb276
Tiago Mendes <tetigo@gmail.com>
2019-10-03 16:34:40 -0300

    Adicionado Paginas Estaticas e Rotas

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const bodyParse = require('body-parser')
+const bodyParser = require('body-parser')
+
+const pages = require('./routes/pages')
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 //prepara pra receber valores no POST
-app.use(bodyParse.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 //lugar dos assets usados no projeto
 app.use(express.static(path.join(__dirname, 'public')))
@@ -19,9 +21,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 
-app.get('/', (req, res)=>{
-    res.send('ok123')
-})
+app.use('/', pages)
 
 mongoose
     .connect(mongo, {useNewUrlParser: true, useUnifiedTopology: true})
